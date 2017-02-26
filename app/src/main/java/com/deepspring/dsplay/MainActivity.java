e0f589c7e8bf55a8c22a8c1cc15f0e47636582d0
vanity <754458151@qq.com>
2017-02-26 19:27:01 +0800

    deemo

--- app/src/main/java/com/deepspring/dsplay/MainActivity.java
+++ app/src/main/java/com/deepspring/dsplay/MainActivity.java
@@ -1,13 +1,80 @@
 package com.deepspring.dsplay;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+    private View headerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                Log.d("MainActivity","Slide");
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
+        headerView = mNavigationView.getHeaderView(0);
+        headerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"headerView clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.menu_app_update:
+                        Toast.makeText(MainActivity.this,"应用更新",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_app_message:
+                        Toast.makeText(MainActivity.this,"应用信息",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_setting:
+                        Toast.makeText(MainActivity.this,"应用设置",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
