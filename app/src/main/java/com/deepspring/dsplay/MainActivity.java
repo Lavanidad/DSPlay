b7d9d9b89d5bf23202efc41af6d20a2ab9683051
vanity <754458151@qq.com>
2017-02-26 20:02:20 +0800

    add toolbar

--- app/src/main/java/com/deepspring/dsplay/MainActivity.java
+++ app/src/main/java/com/deepspring/dsplay/MainActivity.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout mDrawerLayout;
     @BindView(R.id.navigation_view)
     NavigationView mNavigationView;
+    @BindView(R.id.tool_bar)
+    Toolbar mToolBar;
 
     private View headerView;
 
@@ -27,28 +30,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                Log.d("MainActivity","Slide");
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-
-            }
-        });
 
         headerView = mNavigationView.getHeaderView(0);
         headerView.setOnClickListener(new View.OnClickListener() {
@@ -76,5 +57,13 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        //toolbar
+        mToolBar.inflateMenu(R.menu.toolbar_menu);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolBar,
+                R.string.open,R.string.close);
+        drawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(drawerToggle);
+
+
     }
 }
