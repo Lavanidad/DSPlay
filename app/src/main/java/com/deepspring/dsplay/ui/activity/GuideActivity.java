fa9ef4b3e1ce1cb16ede753a543753c46df28eda
vanity <754458151@qq.com>
2017-03-24 23:55:18 +0800

    3.2.1 --add todo

--- app/src/main/java/com/deepspring/dsplay/ui/activity/GuideActivity.java
+++ app/src/main/java/com/deepspring/dsplay/ui/activity/GuideActivity.java
@@ -5,13 +5,14 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.deepspring.dsplay.R;
 import com.deepspring.dsplay.ui.adapter.GuideFragmentAdapter;
 import com.deepspring.dsplay.ui.fragment.GuideFragment;
+import com.deepspring.dsplay.ui.widget.CircleIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +30,7 @@ public class GuideActivity extends AppCompatActivity implements ViewPager.OnPage
     @BindView(R.id.btn_enter)
     Button mBtnEnter;
     @BindView(R.id.indicator)
-    LinearLayout indicator;
+    CircleIndicator mIndicator;
     @BindView(R.id.activity_guide)
     RelativeLayout mActivityGuide;
 
@@ -61,11 +62,15 @@ public class GuideActivity extends AppCompatActivity implements ViewPager.OnPage
         mViewpager.setCurrentItem(0);
         mViewpager.setOffscreenPageLimit(mAdapter.getCount());
         mViewpager.setAdapter(mAdapter);
+        mViewpager.addOnPageChangeListener(this);
+        //mViewpager.setPageTransformer();
+        mIndicator.setViewPager(mViewpager);
+        //mIndicator.configureIndicator();
     }
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+        mBtnEnter.setVisibility((position == mAdapter.getCount()-1)? View.VISIBLE:View.GONE);
     }
 
     @Override
@@ -77,4 +82,11 @@ public class GuideActivity extends AppCompatActivity implements ViewPager.OnPage
     public void onPageScrollStateChanged(int state) {
 
     }
+
+//    @OnClick(R.id.btn_enter)
+//    public void onClick() {
+//        ACache.get(this).put(Constant.IS_SHOW_GUIDE,"0");
+//        startActivity(new Intent(this,MainActivity.class));
+//        this.finish();
+//    }
 }
