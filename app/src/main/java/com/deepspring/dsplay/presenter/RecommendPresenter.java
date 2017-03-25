250adec4b667ea392e5e2ff8614a35a8e8ad235b
vanity <754458151@qq.com>
2017-03-25 22:35:44 +0800

    3.3.1 --an error

--- app/src/main/java/com/deepspring/dsplay/presenter/RecommendPresenter.java
+++ app/src/main/java/com/deepspring/dsplay/presenter/RecommendPresenter.java
@@ -1,8 +1,10 @@
 package com.deepspring.dsplay.presenter;
 
+import android.util.Log;
+
 import com.deepspring.dsplay.bean.AppInfo;
-import com.deepspring.dsplay.bean.BaseBean;
 import com.deepspring.dsplay.bean.PageBean;
+import com.deepspring.dsplay.bean.BaseBean;
 import com.deepspring.dsplay.common.rx.RxHttpReponseCompat;
 import com.deepspring.dsplay.data.RecommendModel;
 import com.deepspring.dsplay.presenter.contract.RecommendContract;
@@ -10,10 +12,6 @@ import com.deepspring.dsplay.presenter.contract.RecommendContract;
 import javax.inject.Inject;
 
 import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
-
-import static com.deepspring.dsplay.common.rx.RxHttpReponseCompat.compatResult;
 
 /**
  * Created by Anonym on 2017/3/6.
@@ -30,7 +28,13 @@ public class RecommendPresenter extends BasePresenter<RecommendModel,RecommendCo
 
         mModel.getApps()
                 .compose(RxHttpReponseCompat.<PageBean<AppInfo>>compatResult())
+                //.compose(RxHttpReponseCompat.<PageBean<AppInfo>>compatResult())
                 .subscribe(new Subscriber<PageBean<AppInfo>>() {
+                    @Override
+                    public void onStart() {
+                        mView.showLoading();
+                    }
+
                     @Override
                     public void onCompleted() {
 
@@ -38,13 +42,17 @@ public class RecommendPresenter extends BasePresenter<RecommendModel,RecommendCo
 
                     @Override
                     public void onError(Throwable e) {
-
+                        Log.d("RecPresent", "error");
                     }
 
                     @Override
                     public void onNext(PageBean<AppInfo> appInfoPageBean) {
-
+                        if (appInfoPageBean != null) {
+                            mView.showResult(appInfoPageBean.getDatas());
+                        } else {
+                            mView.showNodata();
+                        }
                     }
-                });
+                })
     }
 }
