3c119688e5e2e530748004a4b0c6dfd35cfe7e9c
vanity <754458151@qq.com>
2017-04-06 20:19:37 +0800

    3.3.2 -- test1.0

--- app/src/main/java/com/deepspring/dsplay/common/rx/subscriber/DefaultSubscriber.java
+++ app/src/main/java/com/deepspring/dsplay/common/rx/subscriber/DefaultSubscriber.java
@@ -3,9 +3,8 @@ package com.deepspring.dsplay.common.rx.subscriber;
 import rx.Subscriber;
 
 /**
- * Created by Anonym on 2017/3/27.
+ * Created by Anonym on 2017/4/6.
  */
 
 public abstract class DefaultSubscriber<T> extends Subscriber<T> {
-
 }
