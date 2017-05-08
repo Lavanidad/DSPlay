accb3ebb1d701d75a9858adf13e68edde5eb7d9e
vanity <754458151@qq.com>
2017-05-08 21:31:32 +0800

    4.1.5 -- test

--- app/src/main/java/com/deepspring/dsplay/ui/activity/LoginActivity.java
+++ app/src/main/java/com/deepspring/dsplay/ui/activity/LoginActivity.java
@@ -1,14 +1,17 @@
 package com.deepspring.dsplay.ui.activity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.deepspring.dsplay.R;
 import com.deepspring.dsplay.di.component.AppComponent;
+import com.tbruyelle.rxpermissions.RxPermissions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.functions.Action1;
 
 /**
  * Created by Anonym on 2017/4/9.
@@ -49,56 +52,44 @@ public class LoginActivity extends BaseActivity {
     public void onClick() {
 
 
-//        RxPermissions rxPermissions = new RxPermissions(this);
-//
-//        rxPermissions.request(Manifest.permission.CAMERA)
-//                .subscribe(new Action1<Boolean>() {
-//                    @Override
-//                    public void call(Boolean aBoolean) {
-//                        if(aBoolean){
-//
-//
-//                        }else {
-//
-//                        }
-//
-//                    }
-//                });
+        RxPermissions rxPermissions = new RxPermissions(this);
 
-//
+        rxPermissions.request(Manifest.permission.CAMERA)
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean aBoolean) {
+                        if(aBoolean){
+
+                        }else {
+                        }
+                    }
+                });
+
 //        // 没有授权
-//
 //        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
-//
-//
 //            ActivityCompat.requestPermissions(this,new String[]{ Manifest.permission.READ_PHONE_STATE},READ_PHONE_STATE_CODE);
-//
 //        }
 //        else{
-//
 //            // 已经授权
-//
 //            String imei = DeviceUtils.getIMEI(this);
-//            Toast.makeText(LoginActivity.this,"imei="+imei,Toast.LENGTH_LONG).show();
+//            Toast.makeText(LoginActivity.this,"imei="+imei, Toast.LENGTH_LONG).show();
 //        }
 //    }
-
+//
 //    @Override
 //    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 //
 //        if(requestCode == READ_PHONE_STATE_CODE){
 //
 //
-//            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+//            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
 //
-//                String imei  =DeviceUtils.getIMEI(this);
+//                String imei  = DeviceUtils.getIMEI(this);
 //                Toast.makeText(LoginActivity.this,"imei="+imei,Toast.LENGTH_LONG).show();
 //            }
 //            else {
 //                Toast.makeText(LoginActivity.this,"用户拒绝授权",Toast.LENGTH_LONG).show();
 //            }
-//
 //        }
-//
     }
 }
